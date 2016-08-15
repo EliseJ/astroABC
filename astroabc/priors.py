2132f86d68ad402ba76e4985cea09865d6b74ad5
elise <elise@elises-iMac.local>
2016-08-15 18:47:53 -0500

    nonstandard priors

--- astroabc/priors.py
+++ astroabc/priors.py
@@ -2,6 +2,8 @@ import numpy as np
 import scipy.stats
 import time
 
+NFILES = 5
+FILENAME = "omw_" # prefix for files followed by filenumber
 
 class Prior_class(object):
         '''Prior class''' 
@@ -13,6 +15,38 @@ class Prior_class(object):
 		'''
                 self.priorname=priorname
                 self.hyperparams = hyperparams
+		if self.priorname == "nonstandard": #first hyperparam = column from file to be read, specify filename and number of files above
+			'''useful for sampling from non-standard discrete pdf e.g. Planck/WMAP chain'''
+			self.read_data(hyperparams[0])
+			self.inv_transform_spline()
+			self.pdf_spline()
+
+	def read_data(self,colnum):
+		'''Only for "nonstandard". Method to read discrete pdf for parameter from file
+		Input: colnum: column number to be read from file
+		'''
+		self.param=[]
+		for i in range(1,NFILES):
+			d = np.loadtxt(FILENAME+str(i)+".txt")
+			for j in range(len(d[:,colnum])):
+				self.param.append(d[:,colnum][j])
+
+	def inv_transform_spline(self):
+		'''Only for "nonstandard". Method to create inverse spline to discrete cumulative distribution function 
+		to allow drawing random variables.
+		Warning: user should check that spline faithfully matches actual cdf.
+		'''
+		srt_param=np.sort(self.param)
+		cdf = np.array(range(len(self.param)))/float(len(self.param))
+		#create a spline
+		self.spline2_cdf = UnivariateSpline(cdf,srt_param,k=5)
+
+	def pdf_spline(self):
+		'''Only for "nonstandard". Method creates a spline to the normalised PDF for discrete parameter values.
+		Warning: user should check that spline faithfully matches actual pdf.
+		'''
+		hist,nbins = np.histogram(self.param,normed=True,bins=200)
+		self.spline2_pdf = interp1d(nbins[1:],hist)
         
         def return_priorprob(self,value):
 		'''Input:
@@ -28,6 +62,8 @@ class Prior_class(object):
 		elif self.priorname =="uniform":
 			width = self.hyperparams[1] - self.hyperparams[0]
                         return scipy.stats.uniform.pdf(value, loc = self.hyperparams[0],scale=width)
+		elif self.priorname == "nonstandard":
+			return self.spline2_pdf(value)
 
 
         def prior(self):
@@ -43,4 +79,9 @@ class Prior_class(object):
                         return float(np.random.normal(self.hyperparams[0],self.hyperparams[1],size=1))
                 elif self.priorname =="uniform":
                         return float(np.random.uniform(low=self.hyperparams[0],high=self.hyperparams[1],size=1))
+		elif self.priorname == "nonstandard":
+			uni_rvs = np.random.uniform()
+			return float(self.spline2_cdf(uni_rvs))
+
+
 
