e58178b7fde4fc2e00111c75087d0301320eed7d
elise <elise@elises-iMac.local>
2016-08-16 13:20:45 -0500

    reconfigured __init__

--- astroabc/__init__.py
+++ astroabc/__init__.py
@@ -2,3 +2,12 @@ __author__ = 'Elise Jennings'
 __email__ = 'elise@fnal.gov'
 __version__ = '0.0.0'
 
+from .abc_class import *
+from .io_utils import *
+from .model import *
+from .mpi_pool import *
+from .myutils import *
+from .priors import *
+from .tolerance import *
+from .variance import *
+
