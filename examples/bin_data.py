ff3feb6d49bb60f16b2bafa7f2f51f404f9c642e
elise jennings <elise@elises-MacBook-Air.local>
2016-08-24 17:30:30 -0500

    adding supernove notebook

---
+++ examples/bin_data.py
@@ -0,0 +1,48 @@
+import numpy as np
+import math
+
+def read_fitres(fname):
+                dtype=[('z',np.float32),('mu',np.float32),('mu_err',np.float32)]
+                data=np.loadtxt(fname,skiprows = 15,dtype =dtype, usecols=(34,48,50))
+                return data
+
+def bin_data(z,mu,err,Nbins=10):
+    binw = (z.max()-z.min())/Nbins
+    #print binw
+    zbins = np.arange(z.min(),z.max(),binw)
+    mu_in_bin=[];err_in_bin=[]
+    avmu_bin=[] ; averr_bin=[]
+    for i in range(Nbins):
+        mu_in_bin.append([]); err_in_bin.append([])
+    for i,m in enumerate(mu):
+        for j in range(Nbins): 
+            if z[i] >=z.min()+ j*binw and z[i] < z.min() + (j+1)*binw:
+                mu_in_bin[j].append(m) ; err_in_bin[j].append(err[i])
+    for i in range(Nbins):
+        if mu_in_bin[i]:
+            avmu_bin.append(np.mean(mu_in_bin[i]))
+        else: avmu_bin.append(0)
+        if err_in_bin[i]:
+            averr_bin.append(np.mean(err_in_bin[i]))
+        else: averr_bin.append(0)
+    return  zbins, avmu_bin,averr_bin,mu_in_bin,mu_in_bin
+
+def read_data():
+	data = read_fitres("OUT2_SALT2mu+wOL.fitres")
+
+	#Keep data points with z>0.5
+	z_new = data['z'][0]
+	mu_new = data['mu'][0]
+	err_new = data['mu_err'][0]
+
+	for i in range(1,345):
+	    if data['z'][i] >= 0.5:
+        	z_new = np.append(z_new,data['z'][i])
+        	mu_new = np.append(mu_new,data['mu'][i])
+		err_new = np.append(err_new,data['mu_err'][i])
+
+	#bin this data
+	zbins,avmu_bin,averr_bin,mu_in_bin_new,mu_in_bin_new = bin_data(z_new,mu_new,err_new,Nbins=20)
+	return zbins,avmu_bin,averr_bin,mu_in_bin_new,mu_in_bin_new
+
+
---
+++ examples/distance_calc.py
@@ -0,0 +1,69 @@
+#Author:Elise Jennings 
+#elise@fnal.gov 
+
+import numpy as np
+import scipy.integrate
+c_km_per_s = 299792.458
+
+class DistanceCalc(object):
+    def __init__(self,om,ok,ol,wmodel,de_params,h0):
+        self.om=om
+        self.ol=ol
+        self.ok=ok
+        if self.om + self.ol ==1.: self.is_flat = True
+	self.wmodel=wmodel
+        self.de_params=de_params
+        self.h0=h0
+        self.d_h =  c_km_per_s/(100.) #Mpc/h
+
+    def wfunc(self,a):
+	if self.wmodel == -1:
+            w0 = self.de_params
+            return w0
+        elif self.wmodel == 0: # e.g. Linder 2003
+            w0,wa=self.de_params
+            return w0 + (1.0-a)*wa
+        elif self.wmodel == 1: # e.g Huterer & Turner 2001
+            w0,wa,ap=self.de_params
+            return w0 + (ap-a)*wa
+        elif self.wmodel ==2: # Wetterich 2004
+            w0,ode_e = self.de_params
+            b = -3.*(  w0/( np.log((1.-ode_e)/ode_e) + np.log((1.-self.om)/self.om)  ))
+            return w0/(1.0 - b*(np.log(a)))
+            
+
+    def w_integrand(self,a):
+        return (1.+ self.wfunc(a))/a
+
+    def w_int(self,z):
+        a=1./(1.+z)
+        return 3.0*scipy.integrate.quad(self.w_integrand,a,1.0,epsrel=1e-6,limit=50)[0]
+
+    def e_z_inverse(self,z):
+	if self.wmodel == -1:
+        	return 1./(np.sqrt(self.om*(1+z)**3 + self.ok*(1+z)**2 + self.ol))
+	else:
+        	return 1./(np.sqrt(self.om*(1+z)**3 + self.ok*(1+z)**2 + self.ol*np.exp(self.w_int(z))))
+
+    def d_c(self,z):
+        return self.d_h*scipy.integrate.quad(self.e_z_inverse,0.0,z,epsrel=1e-6,limit=50)[0]
+
+
+    def d_m(self,z):
+        '''returns los comoving distance in Mpc/h '''
+       	return self.d_c(z)
+
+    def hubble(self,z):
+        return 100.*self.h0*1./self.e_z_inverse(z)
+
+    def d_l(self,z):
+        '''luminosity dist in Mpc '''
+        return (1.+z)*self.d_m(z)/self.h0
+
+    def d_a(self,z):
+        '''Ang diameter distance in Mpc'''
+        return self.d_m(z)/(1.+z)/self.h0
+
+    def mu(self,z):
+	'''distance modulus'''
+        return 5.*np.log10(self.d_l(z)*1.E6/10.) 
